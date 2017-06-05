3bc6732d0f61d5cb07bb2518cb6fe41d69e7be14
Nattaphol Srisa <rootncdev2015@gmail.com>
2017-06-06 01:47:01 +0700

    first commit

---
+++ Test.js
@@ -0,0 +1,9 @@
+Math.sin(3.5); // Math object
+var circumference = 2 Math.PI * r; //varialbe decarations
+
+// convert a string to an int
+parseInt('123',10); // 123
+parseInt('010',10); // 10
+
+parseInt('010'); // 8
+paseInt('0x10'); // 16
